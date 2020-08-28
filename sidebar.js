c5265db5187136dc6e2bef1c1ca8a8ea5bd5ed90
Vlad <vladyslav.priad@gmail.com>
2020-08-28 22:09:35 +0300

    Text appears

--- sidebar.js
+++ sidebar.js
@@ -2,7 +2,6 @@ const burgerMenu = document.getElementById('burger_link')
 const sidebarBg = document.querySelector('.sidebar_bg')
 const sidebarContent = document.querySelector('.sidebar_content')
 const closeBtn = document.getElementById('close_btn')
-const sidebarContentNextSlide = document.querySelector('.hmenu-content-to-left')
 
 burgerMenu.onclick = function(event) {
     sidebarBg.classList.add('open')
@@ -13,13 +12,30 @@ closeBtn.onclick = function(event) {
     sidebarBg.classList.remove('open')
     sidebarContent.classList.remove('open')
     burgerMenu.classList.remove('hide')
-    sidebarContentNextSlide.classList.remove('open')
+    currentSubcategory.classList.remove('open')
 }
+  
+const subcategories = document.querySelectorAll('.hmenu-translateX-right');
+let currentSubcategory;
 
-const aHref = document.querySelectorAll('.hmenu-content a')
+const back = document.querySelectorAll('hmenu-back-button');
+back.forEach(item => {
+    item.addEventListener('click', (event) => {
+    sidebarBg.classList.remove('open')
+    sidebarContent.classList.remove('open')
+    burgerMenu.classList.remove('hide')
+    currentSubcategory.classList.remove('open')
+    currentSubcategory.classList.add('hide')
+    })
+})
+
+const aHref = document.querySelectorAll('.hmenu-content a');
     aHref.forEach(item => {
         item.addEventListener('click', (event) => {
-            sidebarContentNextSlide.classList.add('open')
+            currentSubcategory = subcategories[item.id-1];
+
+            currentSubcategory.classList.add('open')
+            sidebarContent.classList.add('hide');
             
         })
     })

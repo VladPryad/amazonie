97615efb18e7945daeb0a0e7cfe6fcd8e8499125
Vlad <vladyslav.priad@gmail.com>
2020-08-29 01:00:15 +0300

    main menu display issue fixed

--- sidebar.js
+++ sidebar.js
@@ -3,19 +3,12 @@ const sidebarBg = document.querySelector('.sidebar_bg')
 const sidebarContent = document.querySelector('.sidebar_content')
 const closeBtn = document.getElementById('close_btn')
 const submenu = document.querySelector('.submenu')
-const menu = document.querySelector('under-sign-in')
+const menu = document.querySelector('.under-sign-in')
 
 burgerMenu.onclick = function(event) {
     sidebarBg.classList.add('open')
     sidebarContent.classList.add('open')
     event.target.classList.add('hide')
-}
-closeBtn.onclick = function(event) {
-    sidebarBg.classList.remove('open')
-    sidebarContent.classList.remove('open')
-    burgerMenu.classList.remove('hide')
-    currentSubcategory.classList.remove('open')
-    submenu.classList.remove('open')
 }
   
 const subcategories = document.querySelectorAll('.hmenu-translateX-right');
@@ -34,9 +27,18 @@ const aHref = document.querySelectorAll('.hmenu-content a');
             currentSubcategory = subcategories[item.id-1];
 
             submenu.classList.add('open');
-            //currentSubcategory.classList.add('chosen')
             currentSubcategory.classList.add('open')
-            menu.classList.add('hide')
-            
+            currentSubcategory.style.display = 'inline';
+            menu.style.display = 'none';
         })
     })
+
+    closeBtn.onclick = function(event) {
+        sidebarBg.classList.remove('open')
+        sidebarContent.classList.remove('open')
+        burgerMenu.classList.remove('hide')
+        currentSubcategory.classList.remove('open')
+        submenu.classList.remove('open')
+        currentSubcategory.style.display = 'none';
+        menu.style.display = 'block';
+    }
